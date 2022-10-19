5aad6daf43b16a37005f2a86b9c746a82b70b97b
E. Camarena <ec.worktools@gmail.com>
2022-10-18 22:11:03 -0500

    Guardian upgrade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CalibrationCertificateDownloadComponent } from './components/calibration-certificate-download/calibration-certificate-download.component';
+import { CalibrationCertificateHistoryComponent } from './components/calibration-certificate-history/calibration-certificate-history.component';
+import { DeliveryNoteCreationComponent } from './components/delivery-note-creation/delivery-note-creation.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { MeasuringEquipmentInstallationComponent } from './components/measuring-equipment-installation/measuring-equipment-installation.component';
+import { TestEquipmentMonitoringComponent } from './components/test-equipment-monitoring/test-equipment-monitoring.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent , canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent , canActivate: [AuthGuard] },
+  { path: 'certificate-download', component: CalibrationCertificateDownloadComponent , canActivate: [AuthGuard] },
+  { path: 'certificate-history', component: CalibrationCertificateHistoryComponent , canActivate: [AuthGuard] },
+  { path: 'delivery-note', component: DeliveryNoteCreationComponent , canActivate: [AuthGuard] },
+  { path: 'equipment-installation', component: MeasuringEquipmentInstallationComponent , canActivate: [AuthGuard] },
+  { path: 'test-equipment', component: TestEquipmentMonitoringComponent , canActivate: [AuthGuard] },
   { path: '**', pathMatch: 'full', redirectTo: 'login' }
   
 ];
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   constructor(
+    private JwtHelper: JwtHelperService
   ){
     
   }
   canActivate():boolean{
+    if(!this.isAuth()){
+      console.log("Token no es válido o ya expiró")
+      return false;
+    }
     return true;
   }
 
   isAuth():boolean{
-    const token = localStorage.getItem('token');
-    if(!localStorage.getItem('token')){
+    const token = localStorage.getItem('token') ?? '';
+    if(this.JwtHelper.isTokenExpired(token) || !localStorage.getItem('token') || token==''){
       return false;
     }
     return true;
