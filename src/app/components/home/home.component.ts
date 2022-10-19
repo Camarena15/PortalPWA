8f3ebdfa02976338a4440dd4fa17d7e9e296e43a
Manuel Figueroa <figueroagarciamanuel@gmail.com>
2022-10-18 21:47:43 -0500

    Mostrar y ocultar menus

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,5 +12,22 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  asb ()  {
+    var burger = <HTMLElement>document.querySelector('.burger');
+    
+    var menu = <HTMLElement> document.querySelector('#'+burger.dataset["target"]);
+    
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    
+  }
+  viewMenu (){
+    let menu2 = <HTMLElement> document.getElementById('menu2');
+    menu2.classList.toggle('is-hidden-mobile');
+}
+activeStyleItem(activo:boolean){
+  return activo ?
+        {'background': '#3273dc','color': 'white'}:
+        {}
+  }
 }
