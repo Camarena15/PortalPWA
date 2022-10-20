4a78760dfca7025f17fcb5aba2ef556bf8c729c0
E. Camarena <ec.worktools@gmail.com>
2022-10-19 22:26:10 -0500

    LocalStorage Hakai

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -16,8 +16,7 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    const token = localStorage.getItem('token')
-    
+    localStorage.removeItem('token')
   }
 
   logIn(){
